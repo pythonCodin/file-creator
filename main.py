c7a5307b1c7b73a3bcabc5722b7f7fdc62b2c1b7
q923 <79765126+q923@users.noreply.github.com>
2021-06-25 06:01:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ except:
     quit()
 # getting the file name
 filename = input(f'Enter the file name you want to create in {path}: ')
+# asking if they want to write anything
 inp = input('Do you want to write something in the file?: ')
-if inp.lower() == 'no':
+if inp.lower() == 'yes':
+    # what they want to write
+    write_to_file = input('What do you want to write in the file?: ')
+    # creating and writing
+    try:
+        with open(filename, 'w') as filew:
+            filew.write(write_to_file)
+            filew.close()
+    except:
+        print('You can\'t write to {}'.format(filename))
+else:
     quit()
-# what they want to write
-write_to_file = input('What do you want to write in the file?: ')
-# creating and writing
-try:
-    with open(filename, 'w') as filew:
-        filew.write(write_to_file)
-        filew.close()
-except:
-    print('You can\'t write to {}'.format(filename))
