c3f0e4507abfd8e03afce85205200bf40b296072
q923 <79765126+q923@users.noreply.github.com>
2021-06-25 05:59:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+# asking the user for the path
+path = input('Enter the path where you want to create the file: ')
+# changing to the path the user entered
+try:
+    os.chdir(path)
+except:
+    print('Invalid path')
+    quit()
+# getting the file name
+filename = input(f'Enter the file name you want to create in {path}: ')
+inp = input('Do you want to write something in the file?: ')
+if inp.lower() == 'no':
+    quit()
+# what they want to write
+write_to_file = input('What do you want to write in the file?: ')
+# creating and writing
+try:
+    with open(filename, 'w') as filew:
+        filew.write(write_to_file)
+        filew.close()
+except:
+    print('You can\'t write to {}'.format(filename))
